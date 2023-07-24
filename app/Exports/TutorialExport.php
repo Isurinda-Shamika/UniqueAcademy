acfc8d65fd847f414a3eecf932091d4cfa563c3c
Isurinda Shamika <isurindashamika20000524@gmail.com>
2023-07-24 09:06:19 +0530

    main

---
+++ app/Exports/TutorialExport.php
@@ -0,0 +1,19 @@
+#	new file:   app/Exports/TutorialExport.php
+
+<?php
+
+namespace App\Exports;
+
+use App\Models\Tutorial;
+use Maatwebsite\Excel\Concerns\FromCollection;
+
+class TutorialExport implements FromCollection
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+    public function collection()
+    {
+        return Tutorial::all();
+    }
+}
